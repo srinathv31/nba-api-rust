e1fc05dab4b72c90829cadb3da8695700d255205
Srinath Venkatesh <srinathvb1@gmail.com>
2023-12-06 20:54:29 -0500

    refactored to use src/lib, unit test start

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use actix_web::{get, web, Result, Responder, HttpResponse};
+use actix_web::{App, HttpServer};
 
-mod file_actions;
+use nba_api_rust::endpoints::index;
+use nba_api_rust::endpoints::get_team_year;
+use nba_api_rust::endpoints::get_roster;
+use nba_api_rust::endpoints::get_schedule;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_web::{App, HttpServer};
-
     HttpServer::new(|| App::new()
         .service(index)
         .service(get_team_year)
@@ -16,93 +17,3 @@ async fn main() -> std::io::Result<()> {
             .run()
             .await
 }
-
-#[get("/")]
-async fn index() -> Result<impl Responder> {
-    Ok("Hit our endpoints to get NBA Data")
-}
-
-#[get("/v1/nba/{team}/{year}")]
-async fn get_team_year(path: web::Path<(String, String)>) -> Result<impl Responder> {
-    // parse url params
-    let (team, year) = path.into_inner();
-
-    // Grab JSON data
-    let data_result = file_actions::get_json();
-
-    // Parse out data/error
-    let data = match data_result {
-        Ok(data) => data,
-        Err(error) => {
-            println!("ERROR: {:?}", error);
-            return Ok(HttpResponse::InternalServerError().body("API Error"))},
-    };
-
-    // access relevant data
-    let res = &data[&team][&year];
-
-    if res.is_null() {
-        println!("{}", format!("Team Data was null for {} - {}", year, team));
-        let res_msg = format!("Team data for the {} - {} was not found.", year, team);
-        return Ok(HttpResponse::NotFound().body(res_msg));
-    }
-
-    Ok(HttpResponse::Ok().body(res.to_string()))
-}
-
-#[get("/v1/nba/{team}/{year}/roster")]
-async fn get_roster(path: web::Path<(String, String)>) -> Result<impl Responder> {
-    // parse url params
-    let (team, year) = path.into_inner();
-
-    // Grab JSON data
-    let data_result = file_actions::get_json();
-
-    // Parse out data/error
-    let data = match data_result {
-        Ok(data) => data,
-        Err(error) => {
-            println!("ERROR: {:?}", error);
-            return Ok(HttpResponse::InternalServerError().body("API Error"))},
-    };
-
-    // access relevant data
-    let res = &data[&team][&year]["Roster"];
-
-    if res.is_null() {
-        println!("{}", format!("Roster Data was null for {} - {}", year, team));
-        let res_msg = format!("Roster for the {} - {} was not found.", year, team);
-        return Ok(HttpResponse::NotFound().body(res_msg));
-    }
-
-    Ok(HttpResponse::Ok().body(res.to_string()))
-}
-
-#[get("/v1/nba/{team}/{year}/schedule")]
-async fn get_schedule(path: web::Path<(String, String)>) -> Result<impl Responder> {
-    // parse url params
-    let (team, year) = path.into_inner();
-
-    // Grab JSON data
-    let data_result = file_actions::get_json();
-
-    // Parse out data/error
-    let data = match data_result {
-        Ok(data) => data,
-        Err(error) => {
-            println!("ERROR: {:?}", error);
-            return Ok(HttpResponse::InternalServerError().body("API Error"))},
-    };
-
-    // access relevant data
-    let res = &data[&team][&year]["Schedule"];
-
-    if res.is_null() {
-        println!("{}", format!("Schedule Data was null for {} - {}", year, team));
-        let res_msg = format!("Schedule for the {} - {} was not found.", year, team);
-        return Ok(HttpResponse::NotFound().body(res_msg));
-    }
-
-    Ok(HttpResponse::Ok().body(res.to_string()))
-}
-
---
+++ tests/endpoints.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    use actix_web::{http::header::ContentType, test, App};
+
+    use nba_api_rust::endpoints::index;
+
+    #[actix_web::test]
+    async fn test_index_get() {
+        let app = test::init_service(App::new().service(index)).await;
+        let req = test::TestRequest::default()
+            .insert_header(ContentType::plaintext())
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+    }
+}
