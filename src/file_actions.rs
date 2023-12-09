e81ed8d4edb25e78620c46acca9848fc5dca6b16
Srinath Venkatesh <srinathvb1@gmail.com>
2023-12-09 15:00:07 -0500

    reading in json string instead of packaging it

--- src/file_actions.rs
+++ src/file_actions.rs
@@ -1,21 +1,8 @@
-use std::{fs, env, path::Path};
 use serde_json::{Value, Error};
 
+use crate::JSON_CONTENT;
+
 pub fn get_json() -> Result<Value, Box<dyn std::error::Error>> {
-    // Grab JSON file
-    let cargo_manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
-
-    let file_path = Path::new(&cargo_manifest_dir).join("allTeamData.json");
-
-    println!("{}", file_path.display());
-
-    let contents_result = fs::read_to_string(file_path);
-
-    let contents = match contents_result {
-        Ok(contents) => contents,
-        Err(error) => return Err(Box::new(error)),
-    };
-
-    let data: Result<Value, Error> = serde_json::from_str(&contents);
+    let data: Result<Value, Error> = serde_json::from_str(JSON_CONTENT);
     return Ok(data?);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod file_actions;
 pub mod endpoints;
+const JSON_CONTENT: &str = include_str!("../allTeamData.json");
