6c13cccbc592f1d6cbd734e9e33f04ca285796dc
Srinath Venkatesh <srinathvb1@gmail.com>
2023-12-08 19:18:23 -0500

    file fix 2

--- src/file_actions.rs
+++ src/file_actions.rs
@@ -3,7 +3,9 @@ use serde_json::{Value, Error};
 
 pub fn get_json() -> Result<Value, Box<dyn std::error::Error>> {
     // Grab JSON file
-    let file_path = Path::new("./allTeamData.json").canonicalize()?.to_str().unwrap().to_owned();
+    println!("Current working directory: {:?}", std::env::current_dir());
+
+    let file_path = Path::new("allTeamData.json").canonicalize()?.to_str().unwrap().to_owned();
     let contents_result = fs::read_to_string(file_path);
 
     let contents = match contents_result {
